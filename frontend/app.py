0df69fb08136e9ee7c09004d2484824af13ea2ed
JamesOkemwa <okemwasjames@gmail.com>
2026-02-06 12:26:24 +0100

    Add subtitle

--- frontend/app.py
+++ frontend/app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 API_URL = 'http://localhost:8000/search'
 
 st.title("SDI-Search")
-st.markdown('''Searching :blue-background[Spatial Data Infrastructures (SDIs)] using natural language.''')
+st.markdown('''Searching the :blue-background[European Data Portal (EDP)] using natural language.''')
 
 def render_datasets(datasets):
     """Render dataset information in expanders."""
